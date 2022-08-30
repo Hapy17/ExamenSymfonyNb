e3c96137521003118db53e835ac1b9e4b58c1555
Nathan <nathanbadoithb@gmail.com>
2022-08-30 11:02:07 +0200

    front navbar created

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\ListingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home')]
+    public function index(
+        ListingRepository $listingRepository
+    ): Response
     {
+        $listings = $listingRepository->findAll();
+
         return $this->render('front/home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'listings' => $listings
         ]);
     }
 }
